8d58136b5b7d03ba01030e213bbc6817af2a2589
jun <jun@Gram.localdomain>
2026-02-26 22:53:33 +0900

    Tested retrigger

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -3,94 +3,88 @@
 set -e
 set -u
 
-outdir=/tmp/aesd-autograder
-kernel_repo=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-kernel_version=v5.15.163
-busybox_version=1_33_1
-finder_app_dir=$(realpath $(dirname $0))
-arch=arm64
-cross_compile=aarch64-none-linux-gnu-
-sysroot=$(${cross_compile}gcc -print-sysroot)
-
-export arch=${arch}
-export cross_compile=${cross_compile}
+OUTDIR=/tmp/aesd-autograder
+KERNEL_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+KERNEL_VERSION=v5.15.163
+BUSYBOX_VERSION=1_33_1
+FINDER_APP_DIR=$(realpath $(dirname $0))
+ARCH=arm64
+CROSS_COMPILE=aarch64-none-linux-gnu-
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 
 if [ $# -ge 1 ]; then
-	outdir=$1
+    OUTDIR=$1
 fi
 
-mkdir -p ${outdir}
+mkdir -p ${OUTDIR}
 
-cd ${outdir}
-if [ ! -d ${outdir}/linux-stable ]; then
-	git clone ${kernel_repo} --depth 1 --single-branch --branch ${kernel_version} linux-stable
+cd ${OUTDIR}
+if [ ! -d ${OUTDIR}/linux-stable ]; then
+    git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION} linux-stable
 fi
 
-if [ ! -e ${outdir}/linux-stable/arch/${arch}/boot/Image ]; then
-	cd ${outdir}/linux-stable
-	git checkout ${kernel_version}
-	make mrproper
-	make defconfig
-	make -j$(nproc) all
-	make modules
-	make dtbs
+cd ${OUTDIR}/linux-stable
+if [ ! -f arch/${ARCH}/boot/Image ]; then
+    git checkout ${KERNEL_VERSION}
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
-cd ${outdir}
-rm -f Image
-cp ${outdir}/linux-stable/arch/${arch}/boot/Image ${outdir}/
+cp -f ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 
-if [ -d ${outdir}/rootfs ]; then
-	sudo rm -rf ${outdir}/rootfs
+if [ -d ${OUTDIR}/rootfs ]; then
+    sudo -n rm -rf ${OUTDIR}/rootfs || rm -rf ${OUTDIR}/rootfs
 fi
 
-mkdir -p ${outdir}/rootfs
-cd ${outdir}/rootfs
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin var/log
 
-cd ${outdir}
-if [ ! -d ${outdir}/busybox ]; then
-	git clone https://github.com/mirror/busybox.git busybox
-	cd busybox
-	git checkout ${busybox_version}
-	make distclean
-	make defconfig
+cd ${OUTDIR}
+if [ ! -d ${OUTDIR}/busybox ]; then
+    git clone https://github.com/mirror/busybox.git busybox
+    cd busybox
+    git checkout ${BUSYBOX_VERSION}
+    make distclean
+    make defconfig
 else
-	cd busybox
+    cd busybox
 fi
 
-make arch=${arch} cross_compile=${cross_compile}
-make config_prefix=${outdir}/rootfs arch=${arch} cross_compile=${cross_compile} install
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
-cd ${outdir}/rootfs
-${cross_compile}readelf -a bin/busybox | grep "program interpreter"
-${cross_compile}readelf -a bin/busybox | grep "Shared library"
+cd ${OUTDIR}/rootfs
+${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-cp -a ${sysroot}/lib/ld-linux-aarch64.so.1 ${outdir}/rootfs/lib/
-cp -a ${sysroot}/lib64/libm.so.6 ${outdir}/rootfs/lib64/
-cp -a ${sysroot}/lib64/libresolv.so.2 ${outdir}/rootfs/lib64/
-cp -a ${sysroot}/lib64/libc.so.6 ${outdir}/rootfs/lib64/
+cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 lib/
+cp -a ${SYSROOT}/lib64/libm.so.6 lib64/
+cp -a ${SYSROOT}/lib64/libresolv.so.2 lib64/
+cp -a ${SYSROOT}/lib64/libc.so.6 lib64/
 
-sudo mknod -m 666 ${outdir}/rootfs/dev/null c 1 3
-sudo mknod -m 600 ${outdir}/rootfs/dev/console c 5 1
+sudo -n mknod -m 666 dev/null c 1 3 || true
+sudo -n mknod -m 600 dev/console c 5 1 || true
 
-cd ${finder_app_dir}
-make clean
-make cross_compile=${cross_compile}
+cd ${FINDER_APP_DIR}
+make clean || true
+make CROSS_COMPILE=${CROSS_COMPILE}
 
-mkdir -p ${outdir}/rootfs/home
-cp writer ${outdir}/rootfs/home/
-cp finder.sh ${outdir}/rootfs/home/
-cp finder-test.sh ${outdir}/rootfs/home/
-cp autorun-qemu.sh ${outdir}/rootfs/home/
-cp -r conf ${outdir}/rootfs/home/
+mkdir -p ${OUTDIR}/rootfs/home
+cp writer ${OUTDIR}/rootfs/home/
+cp finder.sh ${OUTDIR}/rootfs/home/
+cp finder-test.sh ${OUTDIR}/rootfs/home/
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home/
+cp -r conf ${OUTDIR}/rootfs/home/
 
-sed -i 's#\.\./conf/assignment\.txt#conf/assignment.txt#g' ${outdir}/rootfs/home/finder-test.sh
+sed -i 's#\.\./conf/assignment\.txt#conf/assignment.txt#g' ${OUTDIR}/rootfs/home/finder-test.sh
 
-sudo chown -hR root:root ${outdir}/rootfs
+sudo -n chown -hR root:root ${OUTDIR}/rootfs || true
 
-cd ${outdir}/rootfs
-find . | cpio -H newc -ov --owner root:root > ${outdir}/initramfs.cpio
-cd ${outdir}
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
 gzip -f initramfs.cpio
