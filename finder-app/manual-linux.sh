dfba557b310942413eceb27edc6b319bfae8aac2
jun <jun@Gram.localdomain>
2026-02-26 22:01:30 +0900

    modified

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -3,103 +3,94 @@
 set -e
 set -u
 
-OUTDIR=/tmp/aesd-autograder
-KERNEL_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
-KERNEL_VERSION=v5.15.163
-BUSYBOX_VERSION=1_33_1
-FINDER_APP_DIR=$(realpath $(dirname $0))
-ARCH=arm64
-CROSS_COMPILE=aarch64-none-linux-gnu-
+outdir=/tmp/aesd-autograder
+kernel_repo=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+kernel_version=v5.15.163
+busybox_version=1_33_1
+finder_app_dir=$(realpath $(dirname $0))
+arch=arm64
+cross_compile=aarch64-none-linux-gnu-
+sysroot=$(${cross_compile}gcc -print-sysroot)
 
-if [ $# -lt 1 ]
-then
-	echo "Using default directory ${OUTDIR} for output"
-else
-	OUTDIR=$1
-	echo "Using passed directory ${OUTDIR} for output"
+export arch=${arch}
+export cross_compile=${cross_compile}
+
+if [ $# -ge 1 ]; then
+	outdir=$1
 fi
 
-mkdir -p ${OUTDIR}
+mkdir -p ${outdir}
 
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/linux" ]; then
-    echo "CLONING GIT LINUX REPO SUBMODULE"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION} linux
-fi
-if [ ! -f "${OUTDIR}/linux/arch/${ARCH}/boot/Image" ]; then
-    cd linux
-    echo "Checking out version ${KERNEL_VERSION}"
-    git checkout ${KERNEL_VERSION}
-
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+cd ${outdir}
+if [ ! -d ${outdir}/linux-stable ]; then
+	git clone ${kernel_repo} --depth 1 --single-branch --branch ${kernel_version} linux-stable
 fi
 
-echo "Adding the Image in outdir"
-cp ${OUTDIR}/linux/arch/${ARCH}/boot/Image ${OUTDIR}/
-
-echo "Creating the staging directory for the root filesystem"
-cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
-then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    rm  -rf ${OUTDIR}/rootfs
+if [ ! -e ${outdir}/linux-stable/arch/${arch}/boot/Image ]; then
+	cd ${outdir}/linux-stable
+	git checkout ${kernel_version}
+	make mrproper
+	make defconfig
+	make -j$(nproc) all
+	make modules
+	make dtbs
 fi
 
-mkdir -p ${OUTDIR}/rootfs
-cd ${OUTDIR}/rootfs
+cd ${outdir}
+rm -f Image
+cp ${outdir}/linux-stable/arch/${arch}/boot/Image ${outdir}/
+
+if [ -d ${outdir}/rootfs ]; then
+	sudo rm -rf ${outdir}/rootfs
+fi
+
+mkdir -p ${outdir}/rootfs
+cd ${outdir}/rootfs
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
-mkdir -p usr/bin usr/lib usr/sbin
-mkdir -p var/log
+mkdir -p usr/bin usr/lib usr/sbin var/log
 
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]
-then
-    git clone https://github.com/mirror/busybox.git
-    cd busybox
-    git checkout ${BUSYBOX_VERSION}
-    make distclean
-    make defconfig
+cd ${outdir}
+if [ ! -d ${outdir}/busybox ]; then
+	git clone https://github.com/mirror/busybox.git busybox
+	cd busybox
+	git checkout ${busybox_version}
+	make distclean
+	make defconfig
 else
-    cd busybox
+	cd busybox
 fi
 
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+make arch=${arch} cross_compile=${cross_compile}
+make config_prefix=${outdir}/rootfs arch=${arch} cross_compile=${cross_compile} install
 
-cd ${OUTDIR}/rootfs
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+cd ${outdir}/rootfs
+${cross_compile}readelf -a bin/busybox | grep "program interpreter"
+${cross_compile}readelf -a bin/busybox | grep "Shared library"
 
-SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
-cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
-cp -a ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
-cp -a ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
-cp -a ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
+cp -a ${sysroot}/lib/ld-linux-aarch64.so.1 ${outdir}/rootfs/lib/
+cp -a ${sysroot}/lib64/libm.so.6 ${outdir}/rootfs/lib64/
+cp -a ${sysroot}/lib64/libresolv.so.2 ${outdir}/rootfs/lib64/
+cp -a ${sysroot}/lib64/libc.so.6 ${outdir}/rootfs/lib64/
 
-sudo mknod -m 666 dev/null c 1 3
-sudo mknod -m 600 dev/console c 5 1
+sudo mknod -m 666 ${outdir}/rootfs/dev/null c 1 3
+sudo mknod -m 600 ${outdir}/rootfs/dev/console c 5 1
 
-cd ${FINDER_APP_DIR}
+cd ${finder_app_dir}
 make clean
-make CROSS_COMPILE=${CROSS_COMPILE}
+make cross_compile=${cross_compile}
 
-mkdir -p ${OUTDIR}/rootfs/home/conf
-cp writer ${OUTDIR}/rootfs/home/
-cp finder.sh ${OUTDIR}/rootfs/home/
-cp finder-test.sh ${OUTDIR}/rootfs/home/
-cp autorun-qemu.sh ${OUTDIR}/rootfs/home/
-cp conf/username.txt ${OUTDIR}/rootfs/home/conf/
-cp conf/assignment.txt ${OUTDIR}/rootfs/home/conf/
+mkdir -p ${outdir}/rootfs/home
+cp writer ${outdir}/rootfs/home/
+cp finder.sh ${outdir}/rootfs/home/
+cp finder-test.sh ${outdir}/rootfs/home/
+cp autorun-qemu.sh ${outdir}/rootfs/home/
+cp -r conf ${outdir}/rootfs/home/
 
-sed -i 's#\.\./conf/assignment\.txt#conf/assignment.txt#g' ${OUTDIR}/rootfs/home/finder-test.sh
+sed -i 's#\.\./conf/assignment\.txt#conf/assignment.txt#g' ${outdir}/rootfs/home/finder-test.sh
 
-cd ${OUTDIR}/rootfs
-find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
-cd ${OUTDIR}
+sudo chown -hR root:root ${outdir}/rootfs
+
+cd ${outdir}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${outdir}/initramfs.cpio
+cd ${outdir}
 gzip -f initramfs.cpio
-
-echo "Build complete. Artifacts in ${OUTDIR}"
